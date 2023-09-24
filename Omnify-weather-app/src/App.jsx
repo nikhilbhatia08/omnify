a7c125cd1e7442b3eacca64edf0c6fa5d315c2d9
nikhilbhatia08 <bhatianikhil551@gmail.com>
2023-09-24 13:13:49 +0530

    cmmit for weather app frontend

---
+++ Omnify-weather-app/src/App.jsx
@@ -0,0 +1,183 @@
+import { useRef, useState } from "react";
+
+const d = new Date();
+const c = `${d.getDate}-${d.getMonth}-${d.getFullYear}`
+//[`forecast_weather.${c}.${"06"}.weather.main`]
+
+const flattenObj = (ob) => {
+  let result = {};
+  for (const i in ob) {
+      if ((typeof ob[i]) === 'object' && !Array.isArray(ob[i])) {
+          const temp = flattenObj(ob[i]);
+          for (const j in temp) {
+              result[i + '.' + j] = temp[j];
+          }
+      }
+      else {
+          result[i] = ob[i];
+      }
+  }
+  return result;
+};
+
+const App = () => {
+  const inputRef = useRef(null);
+  const [apiData, setApiData] = useState(null);
+  const [showWeather, setShowWeather] = useState(null);
+
+  const [loading, setLoading] = useState(false);
+
+  const WeatherTypes = [
+    {
+      type: "Clear",
+      img: "https://cdn-icons-png.flaticon.com/512/6974/6974833.png",
+    },
+    {
+      type: "Rain",
+      img: "https://cdn-icons-png.flaticon.com/512/3351/3351979.png",
+    },
+    {
+      type: "Snow",
+      img: "https://cdn-icons-png.flaticon.com/512/642/642102.png",
+    },
+    {
+      type: "Clouds",
+      img: "https://cdn-icons-png.flaticon.com/512/414/414825.png",
+    },
+    {
+      type: "Haze",
+      img: "https://cdn-icons-png.flaticon.com/512/1197/1197102.png",
+    },
+    {
+      type: "Smoke",
+      img: "https://cdn-icons-png.flaticon.com/512/4380/4380458.png",
+    },
+    {
+      type: "Mist",
+      img: "https://cdn-icons-png.flaticon.com/512/4005/4005901.png",
+    },
+    {
+      type: "Drizzle",
+      img: "https://cdn-icons-png.flaticon.com/512/3076/3076129.png",
+    },
+  ];
+
+  const fetchWeather = async () => {
+    const URL = `http://localhost:8085/forecast/${inputRef.current.value}`;
+    setLoading(true);
+    fetch(URL)
+      .then((res) => res.json())
+      .then((data) => {
+        setApiData(null);
+        if (Object.values(data)[1] === 500 || data.cod == 500) {
+          // ARRAY OF OBJ
+          setShowWeather([
+            {
+              type: "Not Found",
+              img: "https://cdn-icons-png.flaticon.com/512/4275/4275497.png",
+            },
+          ]);
+        }
+        else {
+        data = flattenObj(data);
+        for(let i = 0; i < WeatherTypes.length; i++){
+          const weath = Object.values(data)[10];
+          console.log(weath)
+          if(WeatherTypes[i].type === weath){
+            setShowWeather([
+              {
+                type: WeatherTypes[i].type,
+                img: WeatherTypes[i].img,
+              },
+            ]);
+          }
+        }
+        console.log(Object.values(data));
+        console.log(Object.values(data)[10]);
+        console.log(showWeather)
+        setApiData({
+          name: Object.values(data)[10],
+          main:{
+            temp: Object.values(data)[1]
+          }
+        });
+        setLoading(false);
+      }})
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  };
+
+  return (
+    <div className="bg-gray-800 h-screen grid place-items-center">
+      <div className="bg-white w-[500px] p-4 rounded-md">
+        <div className="flex items-center justify-between">
+          <input
+            type="text"
+            ref={inputRef}
+            placeholder="Enter Your Location"
+            className="text-xl border-b
+          p-1 border-gray-200 font-semibold uppercase flex-1"
+          />
+          <button onClick={fetchWeather}>
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/758/758651.png"
+              alt="..."
+              className="w-8"
+            />
+          </button>
+        </div>
+        <div
+          className={`duration-300 delay-75  overflow-hidden
+         ${showWeather ? "h-[27rem]" : "h-0"}`}
+        >
+          {loading ? (
+            <div className="grid place-items-center h-full">
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/1477/1477009.png"
+                alt="..."
+                className="w-14 mx-auto mb-2 animate-spin"
+              />
+            </div>
+          ) : (
+            showWeather && (
+              <div className="text-center flex flex-col gap-6 mt-10">
+                {apiData && (
+                  <p className="text-xl font-semibold">
+                    {apiData?.name}
+                  </p>
+                )}
+                <img
+                  src={showWeather[0]?.img}
+                  alt="..."
+                  className="w-52 mx-auto"
+                />
+                <h3 className="text-2xl font-bold text-zinc-800">
+                  {showWeather[0]?.type}
+                </h3>
+
+                {apiData && (
+                  <>
+                    <div className="flex justify-center">
+                      <img
+                        src="https://cdn-icons-png.flaticon.com/512/7794/7794499.png"
+                        alt="..."
+                        className="h-9 mt-1"
+                      />
+                      <h2 className="text-4xl font-extrabold">
+                        {apiData?.main?.temp}&#176;C
+                      </h2>
+                    </div>
+                  </>
+                )}
+              </div>
+            )
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
